8fc33dd31a020939de869a4fc72d5c4438d169fa
brunoogalvani <bthezolin@gmail.com>
2024-09-05 00:40:25 -0300

    base de dados criada

---
+++ dados.js
@@ -0,0 +1,52 @@
+let dados = [
+    {
+        nome : "LeBron James",
+        descricao : "LeBron James é um dos maiores basquetebolistas de todos os tempos. Conhecido por sua versatilidade, atletismo e liderança, ele conquistou diversos títulos e recordes na NBA.",
+        link : "https://pt.wikipedia.org/wiki/LeBron_James"
+    },
+    {
+        nome : "Stephen Curry",
+        descricao : "Stephen Curry é um dos maiores arremessadores de três pontos da história da NBA. Conhecido por sua habilidade de arremessar de qualquer lugar da quadra, ele revolucionou o jogo com seu estilo de jogo único. Com títulos da NBA e prêmios de MVP, Curry é considerado um dos maiores jogadores da sua geração.",
+        link : "https://pt.wikipedia.org/wiki/Stephen_Curry"
+    },
+    {
+        nome : "Kevin Durant",
+        descricao : "Kevin Durant é um ala-pivô com uma combinação única de altura, habilidade e versatilidade. Conhecido por seu arremesso suave e sua capacidade de pontuar de diversas maneiras, Durant é um dos maiores pontuadores da NBA. Com múltiplos títulos de campeão e prêmios de MVP, Durant é considerado um dos melhores jogadores da liga.",
+        link : "https://pt.wikipedia.org/wiki/Kevin_Durant"
+    },
+    {
+        nome: "Ja Morant",
+        descricao: "Ja Morant é um armador explosivo conhecido por suas enterradas espetaculares e sua habilidade de finalizar jogadas difíceis. Ele rapidamente se tornou um dos jogadores mais emocionantes da NBA e um dos líderes do Memphis Grizzlies.",
+        link: "https://pt.wikipedia.org/wiki/Ja_Morant"
+      },
+      {
+        nome: "Luka Dončić",
+        descricao: "Luka Dončić é um ala-armador esloveno com uma visão de jogo excepcional e uma habilidade de pontuar de média e longa distância. Ele impressionou a NBA com seu talento precoce e se tornou um dos principais jogadores do Dallas Mavericks.",
+        link: "https://pt.wikipedia.org/wiki/Luka_Dončić"
+      },
+      {
+        nome: "Anthony Edwards",
+        descricao: "Anthony Edwards é um ala-armador conhecido por seu atletismo e habilidade de pontuar. Ele foi a primeira escolha geral do draft da NBA e se tornou uma das principais promessas da liga.",
+        link: "https://pt.wikipedia.org/wiki/Anthony_Edwards"
+      },
+      {
+        nome: "Kobe Bryant",
+        descricao: "Kobe Bryant foi um dos maiores jogadores da história da NBA, conhecido por sua ética de trabalho, mamba mentality e cinco títulos de campeão com o Los Angeles Lakers. Ele é considerado um dos maiores pontuadores da liga.",
+        link: "https://pt.wikipedia.org/wiki/Kobe_Bryant"
+      },
+      {
+        nome: "Michael Jordan",
+        descricao: "Michael Jordan é considerado por muitos o maior jogador de basquete de todos os tempos. Com seis títulos da NBA e inúmeros recordes, Jordan é um ícone do esporte e uma inspiração para gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Michael_Jordan"
+      },
+      {
+        nome: "Jayson Tatum",
+        descricao: "Jayson Tatum é um ala completo conhecido por sua versatilidade e habilidade de pontuar. Ele se tornou um dos líderes do Boston Celtics e um dos melhores jogadores jovens da NBA.",
+        link: "https://pt.wikipedia.org/wiki/Jayson_Tatum"
+      },
+      {
+        nome: "Nikola Jokić",
+        descricao: "Nikola Jokić é um pivô sérvio conhecido por sua visão de jogo excepcional, passes precisos e habilidade de pontuar. Ele revolucionou a posição de pivô e liderou o Denver Nuggets a diversos sucessos.",
+        link: "https://pt.wikipedia.org/wiki/Nikola_Jokić"
+      }
+];
