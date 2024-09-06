1b5f93af14d68c737215ad7d8117aa5c14b211e9
brunoogalvani <bthezolin@gmail.com>
2024-09-06 00:43:40 -0300

    criação do código javascript

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log(dados);
+function pesquisar() { // Função que será executada quando o botão de pesquisar for clicado
+    let section = document.getElementById("resultado") // Pega a seção "resultado" do HTML pelo ID
+    console.log(section); // Mostra a variável no console (F12)
+
+    let resultados = "";
+
+    // Para cada dado dentro da lista de dados
+    for (let dado of dados) {
+        resultados += `
+            <div class="item-resultado">
+                <h2>${dado.nome}</h2>
+                <p>${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais Informações</a>
+            </div>
+        `
+    };
+
+    section.innerHTML = resultados; // Adiciona na página HTML a variável "resultados"
+}
