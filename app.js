fd6306df9b591cc5187a2d3f0d5620ff37bb4494
brunoogalvani <bthezolin@gmail.com>
2024-09-06 19:08:15 -0300

    função pesquisar implementada

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
+const botaoPesquisa = document.getElementById('botao-pesquisa');
+
+    document.addEventListener('keypress', function(event) { // Se alguma tecla for pressionada, executa a função "event"
+        if (event.key === 'Enter') { // Se a tecla enter for pressionada
+          botaoPesquisa.click(); // Simula um clique no botão
+        }
+      });
+
 function pesquisar() { // Função que será executada quando o botão de pesquisar for clicado
-    let section = document.getElementById("resultado") // Pega a seção "resultado" do HTML pelo ID
-    console.log(section); // Mostra a variável no console (F12)
+    let section = document.getElementById("resultado"); // Pega a seção "resultado" do HTML pelo ID
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value; // Pega o input "campo-pesquisa" no HTML pelo ID
 
     let resultados = "";
+    let nome = "";
+    let tags = "";
+
+    if (campoPesquisa.trim() === "") { // Se não houver nada escrito
+        section.innerHTML = `
+            <div class="item-resultado nomevalido">
+                <h2>Digite o nome de algum jogador</h2>
+            </div>
+        `
+        return
+    };
+
+    campoPesquisa = campoPesquisa.toLowerCase();
 
     // Para cada dado dentro da lista de dados
     for (let dado of dados) {
-        resultados += `
-            <div class="item-resultado">
-                <h2>${dado.nome}</h2>
-                <p>${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Mais Informações</a>
-            </div>
-        `
+        nome = dado.nome.toLowerCase();
+        tags = dado.tags.toLowerCase();
+
+        if (nome.includes(campoPesquisa) || tags.includes(campoPesquisa)) { // Se o nome ou a tag são iguais a pesquisa
+            // Cria um novo elemento HTML
+            resultados += `
+                <div class="item-resultado">
+                    <h2>${dado.nome}</h2>
+                    <p>${dado.descricao}</p>
+                    <a href=${dado.link} target="_blank">Mais Informações</a>
+                </div>
+            `
+        };
     };
 
+    if (!resultados) { // Se a variável "resultados" estiver vazia
+        resultados =`
+            <div class="item-resultado nomevalido">
+                <h2>Digite um nome válido</h2>
+            </div>
+        `
+    }
     section.innerHTML = resultados; // Adiciona na página HTML a variável "resultados"
 }
--- dados.js
+++ dados.js
@@ -2,51 +2,61 @@ let dados = [
     {
         nome : "LeBron James",
         descricao : "LeBron James é um dos maiores basquetebolistas de todos os tempos. Conhecido por sua versatilidade, atletismo e liderança, ele conquistou diversos títulos e recordes na NBA.",
-        link : "https://pt.wikipedia.org/wiki/LeBron_James"
+        link : "https://pt.wikipedia.org/wiki/LeBron_James",
+        tags: "king james"
     },
     {
         nome : "Stephen Curry",
         descricao : "Stephen Curry é um dos maiores arremessadores de três pontos da história da NBA. Conhecido por sua habilidade de arremessar de qualquer lugar da quadra, ele revolucionou o jogo com seu estilo de jogo único. Com títulos da NBA e prêmios de MVP, Curry é considerado um dos maiores jogadores da sua geração.",
-        link : "https://pt.wikipedia.org/wiki/Stephen_Curry"
+        link : "https://pt.wikipedia.org/wiki/Stephen_Curry",
+        tags: "chef curry"
     },
     {
         nome : "Kevin Durant",
         descricao : "Kevin Durant é um ala-pivô com uma combinação única de altura, habilidade e versatilidade. Conhecido por seu arremesso suave e sua capacidade de pontuar de diversas maneiras, Durant é um dos maiores pontuadores da NBA. Com múltiplos títulos de campeão e prêmios de MVP, Durant é considerado um dos melhores jogadores da liga.",
-        link : "https://pt.wikipedia.org/wiki/Kevin_Durant"
+        link : "https://pt.wikipedia.org/wiki/Kevin_Durant",
+        tags: "kd slim reaper easy money sniper"
     },
     {
         nome: "Ja Morant",
         descricao: "Ja Morant é um armador explosivo conhecido por suas enterradas espetaculares e sua habilidade de finalizar jogadas difíceis. Ele rapidamente se tornou um dos jogadores mais emocionantes da NBA e um dos líderes do Memphis Grizzlies.",
-        link: "https://pt.wikipedia.org/wiki/Ja_Morant"
+        link: "https://pt.wikipedia.org/wiki/Ja_Morant",
+        tags: "black jesus"
       },
       {
         nome: "Luka Dončić",
         descricao: "Luka Dončić é um ala-armador esloveno com uma visão de jogo excepcional e uma habilidade de pontuar de média e longa distância. Ele impressionou a NBA com seu talento precoce e se tornou um dos principais jogadores do Dallas Mavericks.",
-        link: "https://pt.wikipedia.org/wiki/Luka_Dončić"
+        link: "https://pt.wikipedia.org/wiki/Luka_Dončić",
+        tags: "luka doncic tesouro"
       },
       {
         nome: "Anthony Edwards",
         descricao: "Anthony Edwards é um ala-armador conhecido por seu atletismo e habilidade de pontuar. Ele foi a primeira escolha geral do draft da NBA e se tornou uma das principais promessas da liga.",
-        link: "https://pt.wikipedia.org/wiki/Anthony_Edwards"
+        link: "https://pt.wikipedia.org/wiki/Anthony_Edwards",
+        tags: "antman ant"
       },
       {
         nome: "Kobe Bryant",
         descricao: "Kobe Bryant foi um dos maiores jogadores da história da NBA, conhecido por sua ética de trabalho, mamba mentality e cinco títulos de campeão com o Los Angeles Lakers. Ele é considerado um dos maiores pontuadores da liga.",
-        link: "https://pt.wikipedia.org/wiki/Kobe_Bryant"
+        link: "https://pt.wikipedia.org/wiki/Kobe_Bryant",
+        tags: "black mamba mentality"
       },
       {
         nome: "Michael Jordan",
         descricao: "Michael Jordan é considerado por muitos o maior jogador de basquete de todos os tempos. Com seis títulos da NBA e inúmeros recordes, Jordan é um ícone do esporte e uma inspiração para gerações de atletas.",
-        link: "https://pt.wikipedia.org/wiki/Michael_Jordan"
+        link: "https://pt.wikipedia.org/wiki/Michael_Jordan",
+        tags: "goat mj"
       },
       {
         nome: "Jayson Tatum",
         descricao: "Jayson Tatum é um ala completo conhecido por sua versatilidade e habilidade de pontuar. Ele se tornou um dos líderes do Boston Celtics e um dos melhores jogadores jovens da NBA.",
-        link: "https://pt.wikipedia.org/wiki/Jayson_Tatum"
+        link: "https://pt.wikipedia.org/wiki/Jayson_Tatum",
+        tags: "champ"
       },
       {
         nome: "Nikola Jokić",
         descricao: "Nikola Jokić é um pivô sérvio conhecido por sua visão de jogo excepcional, passes precisos e habilidade de pontuar. Ele revolucionou a posição de pivô e liderou o Denver Nuggets a diversos sucessos.",
-        link: "https://pt.wikipedia.org/wiki/Nikola_Jokić"
+        link: "https://pt.wikipedia.org/wiki/Nikola_Jokić",
+        tags: "nikola jokic joker"
       }
 ];
